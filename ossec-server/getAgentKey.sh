e61ec496ca47e18057d4dc27aed4a2ed5b55b526
root <root@testipa.reinoldrojas.com>
2018-05-21 21:11:37 -0400

    first commit

---
+++ ossec-server/getAgentKey.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+
+while getopts ":n:h" optarg;
+do
+  case ${optarg} in 
+    n )
+      SERVER_HOSTNAME=${OPTARG}
+      ;;
+    
+    i )
+      SERVER_IP=${OPTARG}
+      ;;
+
+    h )
+      echo """ $0 will print out the agent key for the server hostname you specify 
+Example: $0 -n testserver
+somereallylongrandomkeythatgetsprintedtostdout
+"""
+  esac
+done
+#add agent to ossec server
+#"${BASEDIR}/addAgent.exp ${SERVER_HOSTNAME} ${SERVER_IP}"
+#print ossec id to log file
+"${BASEDIR}/printID.exp" > "${BASEDIR}/agentList.log"
+#get id of agent
+SERVER_ID=$(grep "Name: ${SERVER_HOSTNAME}" ${BASEDIR}/agentList.log | cut -d"," -f1 | cut -d" " -f 5)
+#print agent key
+${BASEDIR}/printAgentKey.exp ${SERVER_ID} > "${BASEDIR}/agentKey-${SERVER_HOSTNAME}.log"
+echo "$( awk '/Agent key information for/ { getline; print $0; }' ${BASEDIR}/agentKey-${SERVER_HOSTNAME}.log )"
+rm -f "${BASEDIR}/agentKey-${SERVER_HOSTNAME}.log"
+#get agent key
